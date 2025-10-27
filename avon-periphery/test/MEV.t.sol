ea420bfeef3b2f161e417b7e932010fe06dd9d3b
Nabil-Ech <nqbilech01@gmail.com>
2025-10-27 20:21:02 +0100

    oracle maniulation finding submission

--- avon-periphery/test/MEV.t.sol
+++ avon-periphery/test/MEV.t.sol
@@ -1,47 +1,101 @@
 pragma solidity ^0.8.28;
 
+import {Test} from "forge-std/Test.sol";
+import {AvonPool} from "../src/pool/AvonPool.sol";
+import {MyToken} from "./mock/MyToken.sol";
+import {MockUSDC} from "./mock/MockUSDC.sol";
+import {MockOracle} from "./mock/MockOracle.sol";
+import {LinearKinkIRM} from "./mock/LinearKinkIRM.sol";
+import {MockOrderbook} from "./mock/MockOrderbook.sol";
+import {MockOrderbookFactory} from "./mock/MockOrderbookFactory.sol";
+import {AvonPoolFactory} from "../src/factory/AvonPoolFactory.sol";
+import {PoolStorage} from "../src/pool/PoolStorage.sol";
+import {PoolGetter} from "../src/pool/utils/PoolGetter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {TestAvonPool} from "./TestHelper.t.sol";
 import "./BasePoolTest.t.sol";
 import "forge-std/console.sol";
 
-contract MEVTest is BasePoolTest {
-    function setUp() public override {
-        super.setUp();
-        
+contract MEVTest is Test {
+    MockUSDC loanToken;
+    MyToken collateralToken;
+    MockOracle oracle;
+    LinearKinkIRM irm;
+    MockOrderbook orderbook;
+    MockOrderbookFactory orderbookFactory;
+    AvonPoolFactory poolFactory;
+    TestAvonPool pool;
+    TestAvonPool pool1;
+    TestAvonPool pool2;
+
+    address owner = makeAddr("owner");
+    address lender1 = makeAddr("lender1");
+    address lender2 = makeAddr("lender2");
+    address borrower = makeAddr("borrower");
+    address liquidator = makeAddr("liquidator");
+    address manager = makeAddr("manager");
+    address feeRecipient = makeAddr("feeRecipient");
+    address random = makeAddr("random");
+
+    // Constants for testing
+    uint256 constant INITIAL_LOAN_AMOUNT = 10000e6;
+    uint256 constant INITIAL_COLLATERAL_AMOUNT = 10e18;
+    uint256 constant DEFAULT_DEPOSIT_AMOUNT = 1000e6;
+    uint256 constant DEFAULT_BORROW_AMOUNT = 500e6;
+    uint256 constant DEFAULT_COLLATERAL_BUFFER = 0.01e18;
+    uint64 constant MANAGER_FEE = 0.035e18;
+
+    function setUp() public virtual {
+        vm.startPrank(owner);
+
+        // Deploy mocks
+        loanToken = new MockUSDC("Mock USDC", "mUSDC");
+        collateralToken = new MyToken("Mock DAI", "mDAI");
+        oracle = new MockOracle();
+        irm = new LinearKinkIRM(0.01e18, 0.05e18, 0.1e18, 0.8e18);
+        orderbookFactory = new MockOrderbookFactory(feeRecipient);
+        orderbook = new MockOrderbook(address(orderbookFactory));
+
+        // Deploy the real pool factory instead of mock
+        poolFactory = new AvonPoolFactory(address(orderbookFactory));
+
+        // Set up orderbook and pool factory
+        orderbookFactory.mockSetPoolManager(manager, true);
+        orderbookFactory.mockSetOrderbook(address(orderbook));
+        orderbookFactory.mockSetPoolFactory(address(poolFactory), true);
+
+        vm.stopPrank();
+
     }
 
     function testBorrowWithExactAssets() public {
-        // Setup borrow position
-        uint256 borrowAmount = 500e6;
-        uint256 collateralAmount = 1e18;
-        (, uint256 shares) = _setupBorrowingPosition(borrower, borrowAmount, collateralAmount);
+        // Prepare PoolConfig with maliscious Oracle
+        address malisciousOracle = makeAddr("malisciousOracle");
 
-        // 1 year later, repay the borrow
-        vm.warp(block.timestamp + 365 days);
+        PoolStorage.PoolConfig memory cfg = PoolStorage.PoolConfig({
+            loanToken: address(loanToken),
+            collateralToken: address(collateralToken),
+            oracle: malisciousOracle,
+            irm: address(irm),
+            lltv: 0.8e18 // 80% LTV
+        });
 
-        // lender 2 front runs the repay witha flash loan
-        uint256 balanceBefore = loanToken.balanceOf(lender2);
-        vm.startPrank(lender2);
-        uint256 deposit2 = loanToken.balanceOf(lender2);
-        loanToken.approve(address(pool), deposit2);
-        pool.deposit(deposit2, lender2);
-        vm.stopPrank();
+        // Deploy pool using the factory
+        vm.startPrank(manager);
 
-        // Now borrower repays
-        vm.startPrank(borrower);
-        loanToken.approve(address(pool), borrowAmount);
+        pool =
+            TestAvonPool(payable(poolFactory.deployPool(cfg, MANAGER_FEE, 1.03e18, 0.03e18, 0.25e18, 0.001e18, 0, 0))); // No caps
+        cfg.lltv = 0.9e18;
         
-        (uint256 repaidAssets, uint256 repaidShares) = pool.repay(borrowAmount, 0, borrower);
-
         vm.stopPrank();
+        assertTrue(poolFactory.isValidPool(address(pool)), "Pool not valid");
 
-        // lender2 redeems immediately after
-        vm.startPrank(lender2);
-        uint256 sharesToRedeem = pool.balanceOf(lender2);
-        pool.redeem(sharesToRedeem, lender2, lender2);
+        // Whitelist pool in orderbook with maliscious oracle
+        vm.startPrank(manager);
+        orderbook.mockWhitelistPool(address(pool), address(poolFactory));
+        // verifyng that pool was whitelisted with maliscious oracle 
+        assertEq(orderbook.isWhitelisted(address(pool)), true, "Pool not whitelisted");
         vm.stopPrank();
-        uint256 balanceAfter = loanToken.balanceOf(lender2);
-        console.log("Borrower balance diff:", (balanceBefore) / 1e6);
-
 
     }
 
